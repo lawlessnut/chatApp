67ef7835185968eb2707733d1ecc42ca52248d08
Suthar <pchm87@vipuls-mbp.zebra.lan>
2017-10-11 15:50:01 -0700

    socket io added

--- server/server.js
+++ server/server.js
@@ -1,23 +1,25 @@
 const path = require("path");
 const publicPath = path.join(__dirname,"../public");
 const express = require('express');
+const http = require("http");
+const socketIO = require("socket.io");
 
 const PORT = process.env.PORT || 8000;
-
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
 
 app.use(express.static(publicPath));
 
-app.get("/", (req,res)=>{
-    res.render("index");
+io.on("connection",(socket)=>{
+    console.log("new User connected")
+
+    socket.on("disconnect", ()=>{
+        console.log("user was disconnected");
+    })
 })
 
-
-app.listen(PORT,()=>{
+server.listen(PORT,()=>{
     console.log("server started");
 });
-
-
-
-console.log(__dirname+"/../public");
-console.log("")
