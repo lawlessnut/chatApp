ce33a32400bb4a086849104405a8661ad0ae7adc
Suthar <pchm87@vipuls-MacBook-Pro.local>
2017-10-11 18:08:38 -0700

    utility created

---
+++ server/utils/message.test.js
@@ -0,0 +1,12 @@
+const expect = require("expect");
+const { generateMessage } = require("./message");
+
+describe("generateMessage", ()=>{
+    it("should generate correct message object",()=>{
+        var from = "Vipul",
+            text = "Hello",
+            message = generateMessage(from,text);
+        expect(message.createdAt).toBeA("number");
+        expect(message).toInclude({from,text});
+    });
+})
