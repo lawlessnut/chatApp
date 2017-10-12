d0b63f537140dfb633e66452b38c520629715c52
Suthar <pchm87@vipuls-MacBook-Pro.local>
2017-10-11 18:41:10 -0700

    add form and show to message and chat is live

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,6 @@ var socket = io();
 
 socket.on("connect",function(){
     console.log("connect tp server")
-    socket.emit("newMessage",{
-        from : "Bhumi",
-        text : 'Hello Vipul'
-    })
 });
 
 socket.on("disconnect",function(){
@@ -16,4 +12,26 @@ socket.on("disconnect",function(){
 
 socket.on("newMessage", function(message){
     console.log("message",message)
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}:${message.text}`);
+    jQuery("#messages").append(li);
+})
+
+/*
+socket.emit("createMessage",{
+    from : "Frank",
+    text :"Hello, This is Frank"
+}, function(){
+    console.log("Go it");
+})
+*/
+
+jQuery("#message-form").on("submit",(e)=>{
+    e.preventDefault();
+    socket.emit("createMessage",{
+        from :"User",
+        text : jQuery("[name=message]").val()
+    }, function(){
+
+    })
 })
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const publicPath = path.join(__dirname,"../public");
 const express = require('express');
 const http = require("http");
 const socketIO = require("socket.io");
+const { generateMessage } = require("./utils/message");
 
 const PORT = process.env.PORT || 8000;
 const app = express();
@@ -12,11 +13,33 @@ const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+
 io.on("connection",(socket)=>{
     console.log("new User connected")
 
     socket.on("disconnect", ()=>{
         console.log("user was disconnected");
+    })
+
+
+
+    socket.emit("newMessage",generateMessage("Admin", "Welcome to server!"));
+
+    socket.broadcast.emit("newMessage",{
+        from : "Admin",
+        text : "new user joined"
+    })
+    socket.on("createMessage",(message, callback)=>{
+        console.log(message)
+        callback();
+        io.emit("newMessage",generateMessage(message.from, message.text));
+
+
+        /*socket.broadcast.emit("newMessage",{
+            from : message.from,
+            text : message.text,
+            createAt : new Date().getTime()
+        })*/
     })
 })
 
